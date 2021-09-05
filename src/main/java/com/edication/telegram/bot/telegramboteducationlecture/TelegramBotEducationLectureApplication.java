09f37a34181b8c0d739d51da0d202f5a71234196
cypher3301 <cypher.is.3301@gmail.com>
2021-09-05 17:22:20 +0300

    init commit

---
+++ src/main/java/com/edication/telegram/bot/telegramboteducationlecture/TelegramBotEducationLectureApplication.java
@@ -0,0 +1,13 @@
+package com.edication.telegram.bot.telegramboteducationlecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotEducationLectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotEducationLectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edication/telegram/bot/telegramboteducationlecture/TelegramBotEducationLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edication.telegram.bot.telegramboteducationlecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotEducationLectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
