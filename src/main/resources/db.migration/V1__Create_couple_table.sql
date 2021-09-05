e10c0327e3711253cd097f5aedef8a7917b81f5f
cypher3301 <cypher.is.3301@gmail.com>
2021-09-05 20:41:18 +0300

    create flyway migration script V1__Create_couple_table.sql

---
+++ src/main/resources/db.migration/V1__Create_couple_table.sql
@@ -0,0 +1,8 @@
+create table couple(
+                       id int,
+                       name varchar,
+                       auditory varchar,
+                       time varchar,
+                       day varchar,
+                       subweek boolean
+)
